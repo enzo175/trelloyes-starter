57cbfbad3986a41a3de00cfe280cd5494b169b47
Daniel Pali <danielpali@daniels-mbp.home>
2019-10-15 11:37:48 -0700

    with snapshot

--- src/List.test.js
+++ src/List.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
 import List from './List';
+import renderer from 'react-test-renderer';
+// npm install react-test-renderer -D
 
+describe('My component', () => {
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<List />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders the UI as expected', () => {
+  const tree = renderer
+    .create(<List name="list" />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();  
+  });
+
+});
+
+
